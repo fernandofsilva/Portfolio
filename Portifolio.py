86fb23af666d20be78e386c92b3f88449af1b5ae
fernandofsilva <fernandosk8@gmail.com>
2019-01-15 18:18:01 -0200

    First draft

---
+++ Portifolio.py
@@ -0,0 +1,27 @@
+#%%[markdown]
+# ### Fernando Ferreira da Silva
+# email: fernando.f.silva@outlook.com
+# Mobile: +55-11-97692-7927
+#%%[markdown]
+# Web Sites:
+# [Linkedin](https://www.linkedin.com/in/engfernandofsilva)
+# [Github](https://github.com/fernandofsilva)
+# [Kaggle](https://www.kaggle.com/fsilva)
+# [Stackoverflow](https://stackoverflow.com/users/6586216/fernando-silva)
+
+#%%[markdown]
+# ## Education
+# [MSc Computer Engineering](https://www.mackenzie.br/pos-graduacao/mestrado-doutorado/sao-paulo-higienopolis/engenharia-eletrica-e-computacao/)
+# [Universidade Presbiteriana Mackenzie](https://www.mackenzie.br/)
+# jan/2019 - dez/2020
+#
+# [Data Science - Big Data Analytics](https://www.mackenzie.br/pos-graduacao/especializacao/sao-paulo-higienopolis/tecnologia-da-informacao/ciencia-de-dados-big-data-analytics/)
+# [Universidade Presbiteriana Mackenzie](https://www.mackenzie.br/)
+# jan/2015 - jul/2016
+#
+# [Telecommunications Engineering](http://www.unifieo.br/index.php/cursos/graduacao/23-engenharia-de-telecomunicacoes)
+# [UNIFIEO](http://www.unifieo.br/)
+# jan/2007 - dez/2011
+#
+#%%[markdown]
+
